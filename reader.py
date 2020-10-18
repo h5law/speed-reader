14d9f0431a5879cf13732b3f77d2eb4cb14028a2
h5law <h5law@pm.me>
2020-10-18 11:12:10 +0100

    Packaging fixes

--- reader.py
+++ reader.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 from pathlib import Path
 
+
 def file_to_array(path, array):
     print(f"Loading file...")
     with open(path) as f:
@@ -11,33 +12,32 @@ def file_to_array(path, array):
             for word in line.split(" "):
                 array.append(word.strip())
 
-
 def get_word_wait_time(word, wpm):
+    wps = wpm / 60
     if len(word) <= 3:
-        return (0.2)
+        return (1/12) * wps
     else:
-        return ((np.log(len(word)) / ((wpm / 60) * 1.6)))
+        return ((np.log(len(word)) / (wps * 1.45)))
 
-
-def display_line(word, time):
+def display_line(word, time=""):
     click.clear()
     width = click.get_terminal_size()[0]
-    print(f"{word:}{time:>{(width - len(word) - 1)}}s")
-
+    if time != "":
+        time = str(time) + "s"
+    print(f"{word:}{time:>{(width - len(word) - 1)}}")
 
 def read_array(array, wpm):
-    total_words = len(array)
     start_time = time.time()
-    with click.progressbar(length=total_words) as bar:
-        while array[0] is not None:
-            elapsed = time.time() - start_time
-            display_line(array[0], round(elapsed, 2))
-            time.sleep(get_word_wait_time(array[0], wpm))
-            array.pop(0)
-
+    while array:
+        elapsed = time.time() - start_time
+        display_line(array[0], round(elapsed, 2))
+        time.sleep(get_word_wait_time(array[0], wpm))
+        array.pop(0)
+    taken = time.strftime("%Hh:%Mm:%Ss", time.gmtime(elapsed))
+    display_line(f"Read {total_words} words in {taken}.")
 
 @click.command()
-@click.version_option("1.0.1")
+@click.version_option("1.0.4")
 @click.option("--wpm", default=160,
         help="Read at specified WPM, defaults to 160")
 @click.argument("path", required=True, type=Path)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="speed-reader",
-    version="1.0.1",
+    version="1.0.4",
     author="h5law",
     author_email="h5law@pm.me",
     description="A speed reader implemented in Python",
