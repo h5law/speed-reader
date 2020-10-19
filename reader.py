7d1929c5a24b92d92d9768e19366a828d5acbd55
h5law <h5law@pm.me>
2020-10-19 21:01:18 +0100

    Minor update

--- reader.py
+++ reader.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-import click
+import sys
 import time
+import click
 from math import log
 from pathlib import Path
+from os import system, name
 
+
+def clear():
+    if name == "nt":
+        system("cls")
+    else:
+        system("clear")
 
 def file_to_array(path, array):
-    print(f"Loading file...")
     with open(path) as f:
         for line in f.readlines():
             for word in line.split(" "):
@@ -20,28 +27,29 @@ def get_word_wait_time(word, wpm):
         return ((log(len(word), 2)) / (wps * 1.45))
 
 def display_line(word, time=""):
-    click.clear()
     width = click.get_terminal_size()[0]
     if time != "":
         time = str(time) + "s"
     print(f"{word:}{time:>{(width - len(word) - 1)}}")
 
 def read_array(array, wpm):
+    clear()
     start_time = time.time()
-    while array:
+    for word in array:
         elapsed = time.time() - start_time
-        display_line(array[0], round(elapsed, 2))
-        time.sleep(get_word_wait_time(array[0], wpm))
-        array.pop(0)
+        display_line(word, round(elapsed, 2))
+        time.sleep(get_word_wait_time(word, wpm))
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
     taken = time.strftime("%Hh:%Mm:%Ss", time.gmtime(elapsed))
     display_line(f"Read {total_words} words in {taken}.")
 
 @click.command()
-@click.version_option("1.0.4")
+@click.version_option("1.0.5_dev2")
 @click.option("--wpm", default=160,
         help="Read at specified WPM, defaults to 160")
 @click.argument("path", required=True, type=Path)
-def main(path, wpm):
+def read(path, wpm):
     if not path.exists():
         print(f"'{path}' doesn't exist")
         exit(1)
@@ -52,4 +60,4 @@ def main(path, wpm):
 
 
 if __name__ == "__main__":
-    main()
+    read()
